ee11ea6c0921a76e6354bb4957fff2550ea5b3d6
Otto Bruggeman <bruggie@gmail.com>
2001-04-04 00:33:59 +0000

    Hmm finally the preferences work... some halfassed implementation of context diff but that does not work yet anyway... and some small bugfixes... Need to change the color of the background though when ok or apply is pressed in the dialog immediately...
    svn path=/trunk/kdesdk/kompare/; revision=90261

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+			main.cpp  -  description
+			-------------------
+	begin			: Sun Mar 4 2001
+	copyright			: (C) 2001 by Otto Bruggeman
+	email			: otto.bruggeman@home.nl
+***************************************************************************/
+
+/***************************************************************************
+**
+**   This program is free software; you can redistribute it and/or modify
+**   it under the terms of the GNU General Public License as published by
+**   the Free Software Foundation; either version 2 of the License, or
+**   (at your option) any later version.
+**
+****************************************************************************/
 
 #include <kcmdlineargs.h>
 #include <kaboutdata.h>
@@ -13,22 +29,22 @@ static const char *description =
 	
 static KCmdLineOptions options[] =
 {
-  { 0, 0, 0 }
-  // INSERT YOUR COMMANDLINE OPTIONS HERE
+	{ 0, 0, 0 }
+	// INSERT YOUR COMMANDLINE OPTIONS HERE
 };
 
 int main(int argc, char *argv[])
 {
 
-  KAboutData aboutData( "kdiff", I18N_NOOP("KDiff"),
-    "2.0", I18N_NOOP( "A program to view the differences between files with.\n\n"
-			"Many thanks go to Malte Starostic for all the help he has given me." ), KAboutData::License_GPL,
-    "(c) 2001, Otto Bruggeman", 0, 0, "otto.bruggeman@home.nl");
-  aboutData.addAuthor( "Otto Bruggeman", 0, "otto.bruggeman@home.nl" );
-  KCmdLineArgs::init( argc, argv, &aboutData );
-  KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
+	KAboutData aboutData( "kdiff", I18N_NOOP("KDiff"),
+	"2.0", I18N_NOOP( "A program to view the differences between files with.\n\n"
+	"Many thanks go to Malte Starostik for all the help he has given me." ), KAboutData::License_GPL,
+	"(c) 2001, Otto Bruggeman", 0, 0, "otto.bruggeman@home.nl");
+	aboutData.addAuthor( "Otto Bruggeman", 0, "otto.bruggeman@home.nl" );
+	KCmdLineArgs::init( argc, argv, &aboutData );
+	KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
 
-  KDiffApp* kdiffapp = new KDiffApp();
+	KDiffApp* kdiffapp = new KDiffApp();
 
-  return kdiffapp->exec();
+	return kdiffapp->exec();
 }

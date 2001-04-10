c39dc4cd282ccf7d53e4017a07ead00788537d2b
John Firebaugh <jfirebaugh@kde.org>
2001-04-10 21:59:38 +0000

    Adding files needed for new model/view architecture.
    svn path=/trunk/kdesdk/kompare/; revision=91392

---
+++ kompare_shell.cpp
@@ -0,0 +1,171 @@
+/*
+* kdiffshell.cpp
+*
+* Copyright (C) 2001  <kurt@granroth.org>
+*/
+#include "kdiff_shell.h"
+
+#include <kkeydialog.h>
+#include <kconfig.h>
+#include <kurl.h>
+#include <klocale.h>
+#include <kedittoolbar.h>
+#include <kaction.h>
+#include <kstdaction.h>
+#include <klibloader.h>
+#include <kmessagebox.h>
+
+#include "kdiff_part.h"
+#include "kdiffview.h"
+#include "diffmodel.h"
+
+#define ID_N_OF_N      1
+#define ID_GENERAL     2
+
+KDiffShell::KDiffShell()
+	: KParts::MainWindow( 0L, "KDiffShell" )
+{
+	// set the shell's ui resource file
+	setXMLFile("kdiffui.rc");
+
+	// then, setup our actions
+	setupActions();
+	setupStatusBar();
+
+	// and a status bar
+	statusBar()->show();
+
+	// this routine will find and load our Part.  it finds the Part by
+	// name which is a bad idea usually.. but it's alright in this
+	// case since our Part is made for this Shell
+	KLibFactory *factory = KLibLoader::self()->factory("libkdiffpart");
+	if (factory)
+	{
+		// now that the Part is loaded, we cast it to a Part to get
+		// our hands on it
+		m_part = static_cast<KDiffPart *>(factory->create(this,
+		              "kdiff_part", "KParts::ReadWritePart" ));
+
+		if (m_part)
+		{
+			// tell the KParts::MainWindow that this is indeed the main widget
+			setCentralWidget(m_part->widget());
+
+			connect( m_part->diffView(), SIGNAL(itemsChanged()), this, SLOT(updateStatusBar()) );
+			connect( m_part->diffView(), SIGNAL(selectionChanged()), this, SLOT(updateStatusBar()));
+
+			// and integrate the part's GUI with the shell's
+			createGUI(m_part);
+		}
+	}
+	else
+	{
+		// if we couldn't find our Part, we exit since the Shell by
+		// itself can't do anything useful
+		KMessageBox::error(this, "Could not find our Part!");
+		kapp->quit();
+	}
+}
+
+KDiffShell::~KDiffShell()
+{
+}
+
+void KDiffShell::load(const KURL& url)
+{
+	m_part->openURL( url );
+}
+
+void KDiffShell::setupActions()
+{
+	KStdAction::openNew(this, SLOT(fileNew()), actionCollection());
+	KStdAction::quit(kapp, SLOT(quit()), actionCollection());
+
+	m_toolbarAction = KStdAction::showToolbar(this, SLOT(optionsShowToolbar()), actionCollection());
+	m_statusbarAction = KStdAction::showStatusbar(this, SLOT(optionsShowStatusbar()), actionCollection());
+
+	KStdAction::keyBindings(this, SLOT(optionsConfigureKeys()), actionCollection());
+	KStdAction::configureToolbars(this, SLOT(optionsConfigureToolbars()), actionCollection());
+}
+
+void KDiffShell::setupStatusBar()
+{
+	statusBar()->insertItem( i18n(" 0 of 0 differences "), ID_N_OF_N );
+	statusBar()->insertItem( "", ID_GENERAL, 1 );
+}
+
+void KDiffShell::updateStatusBar()
+{
+	QString str;
+	int markedItem = m_part->diffView()->getSelectedItem();
+	int count = m_part->diffView()->getModel()->getDifferenceCount();
+	if (markedItem >= 0)
+		str = i18n(" %1 of %2 differences ").arg(markedItem+1).arg(count);
+	else
+		str = i18n(" %1 differences ").arg(count);
+	statusBar()->changeItem( str, ID_N_OF_N );
+}
+
+void KDiffShell::saveProperties(KConfig* /*config*/)
+{
+	// the 'config' object points to the session managed
+	// config file.  anything you write here will be available
+	// later when this app is restored
+}
+
+void KDiffShell::readProperties(KConfig* /*config*/)
+{
+	// the 'config' object points to the session managed
+	// config file.  this function is automatically called whenever
+	// the app is being restored.  read in here whatever you wrote
+	// in 'saveProperties'
+}
+
+void KDiffShell::fileNew()
+{
+	// this slot is called whenever the File->New menu is selected,
+	// the New shortcut is pressed (usually CTRL+N) or the New toolbar
+	// button is clicked
+
+	// create a new window
+	(new KDiffShell)->show();
+}
+
+
+void KDiffShell::optionsShowToolbar()
+{
+	// this is all very cut and paste code for showing/hiding the
+	// toolbar
+	if (m_toolbarAction->isChecked())
+		toolBar()->show();
+	else
+		toolBar()->hide();
+}
+
+void KDiffShell::optionsShowStatusbar()
+{
+	// this is all very cut and paste code for showing/hiding the
+	// statusbar
+	if (m_statusbarAction->isChecked())
+		statusBar()->show();
+	else
+		statusBar()->hide();
+}
+
+void KDiffShell::optionsConfigureKeys()
+{
+	KKeyDialog::configureKeys(actionCollection(), "kdiff_shell.rc");
+}
+
+void KDiffShell::optionsConfigureToolbars()
+{
+	// use the standard toolbar editor
+	KEditToolbar dlg(actionCollection());
+	if (dlg.exec())
+	{
+		// recreate our GUI
+		createGUI(m_part);
+	}
+}
+
+#include "kdiff_shell.moc"
---
+++ kompare_shell.h
@@ -0,0 +1,76 @@
+#ifndef KDIFFSHELL_H
+#define KDIFFSHELL_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif 
+
+#include <kapp.h>
+#include <kparts/mainwindow.h>
+
+class KToggleAction;
+
+class KDiffPart;
+
+/**
+* This is the application "Shell".  It has a menubar, toolbar, and
+* statusbar but relies on the "Part" to do all the real work.
+*
+* @short Application Shell
+* @author John Firebaugh <jfirebaugh@mac.com>
+* @version 0.1
+*/
+class KDiffShell : public KParts::MainWindow
+{
+	Q_OBJECT
+public:
+	/**
+	* Default Constructor
+	*/
+	KDiffShell();
+
+	/**
+	* Default Destructor
+	*/
+	virtual ~KDiffShell();
+
+	/**
+	* Use this method to load whatever file/URL you have
+	*/
+	void load(const KURL& url);
+
+protected:
+	/**
+	* This method is called when it is time for the app to save its
+	* properties for session management purposes.
+	*/
+	void saveProperties(KConfig *);
+
+	/**
+	* This method is called when this app is restored.  The KConfig
+	* object points to the session management config file that was saved
+	* with @ref saveProperties
+	*/
+	void readProperties(KConfig *);
+
+private slots:
+	void fileNew();
+	void optionsShowToolbar();
+	void optionsShowStatusbar();
+	void optionsConfigureKeys();
+	void optionsConfigureToolbars();
+	void updateStatusBar();
+
+private:
+	void setupAccel();
+	void setupActions();
+	void setupStatusBar();
+
+private:
+	KDiffPart* m_part;
+
+	KToggleAction *m_toolbarAction;
+	KToggleAction *m_statusbarAction;
+};
+
+#endif // KDIFF_H

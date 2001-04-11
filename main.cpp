79fe5637f8471dd02d33ef4658beadfab50f8441
Otto Bruggeman <bruggie@gmail.com>
2001-04-11 17:42:55 +0000

    Some additions to the new model plus implementation of commandline options in the wrapper classes, it is not implemented in the part yet because we are in a major rewrite
    svn path=/trunk/kdesdk/kompare/; revision=91520

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@
 **
 ****************************************************************************/
 
+#include <qcstring.h>
+
 #include <kcmdlineargs.h>
 #include <kaboutdata.h>
 #include <klocale.h>
@@ -23,30 +25,35 @@
 #include "kmaindiffview.h"
 
 static const char *description =
-	I18N_NOOP("A program to view the differences between files.\n\n"
-	          "Many thanks go to Malte Starostik for all the help he has given me.");
+	I18N_NOOP("A program to view the differences between files and optionally generate a diff." );
 
 static const char *version = "v2.0";
 
 static KCmdLineOptions options[] =
 {
-	{ "+[URL]", I18N_NOOP( "Document to open." ), 0 },
+	{ "f format", I18N_NOOP( "The format of the generated diff. It can be one of\nthese: Context, Ed, Normal, RCS, Unified." ), 0 },
+	{ "+[URL [URL2]]", I18N_NOOP( "Document to open. If only one url is given\nit is considered a .diff file. If 2 files are given\nkdiff will create a diff of these files and show it." ), 0 },
 	{ 0, 0, 0 }
 };
 
 int main(int argc, char *argv[])
 {
+	QCString format;
+	KDiffApp* kdiffapp;
 
 	KAboutData aboutData( "kdiff", I18N_NOOP("KDiff"), version, description,
 	                      KAboutData::License_GPL,
 	                      "(c) 2001, Otto Bruggeman", 0, 0, "otto.bruggeman@home.nl");
-	aboutData.addAuthor( "Otto Bruggeman", 0, "otto.bruggeman@home.nl" );
-	aboutData.addAuthor( "John Firebaugh", 0, "jfirebaugh@mac.com" );
-//	about.addAuthor( ); // TODO: add cervisia author
+	aboutData.addAuthor( "Otto Bruggeman", "Author", "otto.bruggeman@home.nl" );
+	aboutData.addAuthor( "John Firebaugh", "Author", "jfirebaugh@mac.com" );
+	aboutData.addCredit( "Malte Starostik", "Big help :)", "malte@kde.org" );
+//	aboutData.addAuthor( ); // TODO: add cervisia author
 	KCmdLineArgs::init(argc, argv, &aboutData);
 	KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
 
-	KDiffApp* kdiffapp = new KDiffApp();
+	KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+	kdiffapp = new KDiffApp( args );
 
 	return kdiffapp->exec();
 }

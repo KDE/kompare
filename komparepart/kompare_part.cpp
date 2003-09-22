f110fba37dc76bc4913f8abb82edd65db4f8603c
Otto Bruggeman <bruggie@gmail.com>
2003-09-22 17:37:11 +0000

    Argh stupid me... it's QStringList::split( sep, string )
    svn path=/trunk/kdesdk/kompare/; revision=253126

--- komparepart/kompare_part.cpp
+++ komparepart/kompare_part.cpp
@@ -223,7 +223,7 @@ bool KomparePart::openDiff( const QString& diffOutput )
 {
 	bool value;
 
-	if ( (value = m_modelList->parseDiffOutput( diffOutput.split( "\n" ) ) ) == true )
+	if ( (value = m_modelList->parseDiffOutput( QStringList::split( "\n", diffOutput ) ) ) == true )
 	{
 		updateActions();
 		updateStatus();

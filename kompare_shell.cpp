22a003bb5a8938ffbb1171640d188826142e5ed3
Otto Bruggeman <bruggie@gmail.com>
2004-03-27 18:44:46 +0000

    Not needed, only clogs the logs
    svn path=/trunk/kdesdk/kompare/; revision=299090

--- kompare_shell.cpp
+++ kompare_shell.cpp
@@ -286,7 +286,7 @@ void KompareShell::slotSetStatusBarText( const QString& text )
 
 void KompareShell::setCaption( const QString& caption )
 {
-	kdDebug() << kdBacktrace();
+//	kdDebug() << kdBacktrace();
 	KParts::DockMainWindow::setCaption( caption, m_viewPart->isModified() );
 }
 

9ce321352257da06be593430e06697e2b45cbcff
Otto Bruggeman <bruggie@gmail.com>
2004-08-28 01:46:02 +0000

    Backport for bug 84671
    svn path=/branches/KDE_3_3_BRANCH/kdesdk/kompare/; revision=342023

--- libdiff2/komparemodellist.cpp
+++ libdiff2/komparemodellist.cpp
@@ -358,7 +358,10 @@ bool KompareModelList::saveDestination( DiffModel* model )
 		QString destination = model->destinationPath() + model->destinationFile();
 		kdDebug(8101) << "Tempfilename   : " << temp->name() << endl;
 		kdDebug(8101) << "DestinationURL : " << destination << endl;
-		result = KIO::NetAccess::upload( temp->name(), KURL( destination ), (QWidget*)parent() );
+		// Cant add a proper error message for this situation due to string freeze in the branch
+		result = KIO::NetAccess::mkdir( KURL( destination ).path(), (QWidget*)parent() );
+		if ( result )
+			result = KIO::NetAccess::upload( temp->name(), KURL( destination ), (QWidget*)parent() );
 	}
 	else
 	{

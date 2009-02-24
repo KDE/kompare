ace780e229f834e6380766443ea46d18bb561726
Kevin Kofler <kevin.kofler@chello.at>
2009-02-24 20:12:27 +0000

    Backport revisions 930812 and 931115 by bruggie from trunk (tested on Fedora 9, KDE 4.2.0): Fix 2 FIXMEs regarding swapping source and destination. (Regression from the fix for remote files.) No need to notify anyone about swapping source with destination. (Remove FIXMEs.)
    svn path=/branches/KDE/4.2/kdesdk/kompare/; revision=931116

--- libdiff2/komparemodellist.cpp
+++ libdiff2/komparemodellist.cpp
@@ -164,8 +164,15 @@ bool KompareModelList::compare()
 		{
 			kDebug(8101) << "Blending source into destination..." << endl;
 			m_info->mode = Kompare::BlendingFile;
-			// FIXME Swap source and destination before calling this
-			// FIXME Do we need to notify anyone we swapped source and destination?
+			// Swap source and destination before calling this
+			KUrl tempUrl( m_info->source );
+			m_info->source = m_info->destination;
+			m_info->destination = tempUrl;
+			QString tempString( m_info->localSource );
+			m_info->localSource = m_info->localDestination;
+			m_info->localDestination = tempString;
+			// Do we need to notify anyone we swapped source and destination?
+			// No we do not need to notify anyone about swapping source with destination
 			result = openFileAndDiff();
 		}
 		else
@@ -183,8 +190,15 @@ bool KompareModelList::compare()
 	else
 	{
 		m_info->mode = Kompare::BlendingDir;
-		// FIXME swap source and destination first in m_info
-		// FIXME Do we need to notify anyone we swapped source and dest?
+		// swap source and destination first in m_info
+		KUrl tempUrl( m_info->source );
+		m_info->source = m_info->destination;
+		m_info->destination = tempUrl;
+		QString tempString( m_info->localSource );
+		m_info->localSource = m_info->localDestination;
+		m_info->localDestination = tempString;
+		// Do we need to notify anyone we swapped source and destination?
+		// No we do not need to notify anyone about swapping source with destination
 		result = openDirAndDiff();
 	}
 

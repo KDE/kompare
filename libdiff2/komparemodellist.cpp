06ed5c943c2a9b20edab4105dc247cd947e8c234
Otto Bruggeman <bruggie@gmail.com>
2009-02-24 20:07:04 +0000

    No need to notify anyone about swapping source with destination.
    svn path=/trunk/KDE/kdesdk/kompare/; revision=931115

--- libdiff2/komparemodellist.cpp
+++ libdiff2/komparemodellist.cpp
@@ -171,7 +171,8 @@ bool KompareModelList::compare()
 			QString tempString( m_info->localSource );
 			m_info->localSource = m_info->localDestination;
 			m_info->localDestination = tempString;
-			// FIXME Do we need to notify anyone we swapped source and destination?
+			// Do we need to notify anyone we swapped source and destination?
+			// No we do not need to notify anyone about swapping source with destination
 			result = openFileAndDiff();
 		}
 		else
@@ -189,14 +190,15 @@ bool KompareModelList::compare()
 	else
 	{
 		m_info->mode = Kompare::BlendingDir;
-		// FIXME swap source and destination first in m_info
+		// swap source and destination first in m_info
 		KUrl tempUrl( m_info->source );
 		m_info->source = m_info->destination;
 		m_info->destination = tempUrl;
 		QString tempString( m_info->localSource );
 		m_info->localSource = m_info->localDestination;
 		m_info->localDestination = tempString;
-		// FIXME Do we need to notify anyone we swapped source and dest?
+		// Do we need to notify anyone we swapped source and destination?
+		// No we do not need to notify anyone about swapping source with destination
 		result = openDirAndDiff();
 	}
 
